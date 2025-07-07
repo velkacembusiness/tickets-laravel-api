1df2498980c4607643826f99effccc6688c08cf0
VelKacem <velkacembusiness@gmail.com>
2025-07-07 14:55:30 +0100

    Testing Postman

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login',[\App\Http\Controllers\AuthController::class,'login']);
+Route::post('/login',[\App\Http\Controllers\AuthController::class,'login']);
+Route::post('/register',[\App\Http\Controllers\AuthController::class,'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
