6ee1c04906a544c8a2f76aeb1c19ae75f6119c51
VelKacem <velkacembusiness@gmail.com>
2025-07-07 15:13:42 +0100

    Define Ticket model & factory

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login',[\App\Http\Controllers\AuthController::class,'login']);
 Route::post('/register',[\App\Http\Controllers\AuthController::class,'register']);
 
+Route::get('/tickets', function () {
+    return \App\Models\Ticket::all();
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
