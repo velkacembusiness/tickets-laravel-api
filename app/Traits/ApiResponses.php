90e14382ff21da87043917b66e6e414f62658f75
VelKacem <velkacembusiness@gmail.com>
2025-07-08 11:07:40 +0100

    Authentication using tokens

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -4,16 +4,22 @@ namespace App\Traits;
 
 trait ApiResponses
 {
-    protected function ok($message)
-    {
-        return $this->success($message, 200);
+    protected function ok($message, $data) {
+        return $this->success($message, $data, 200);
     }
 
-    protected function success($message, $statusCode = 200)
-    {
+    protected function success($message, $data, $statusCode = 200) {
         return response()->json([
-            'message'=> $message,
-            'status'=> $statusCode
+            'data' => $data,
+            'message' => $message,
+            'status' => $statusCode
+        ], $statusCode);
+    }
+
+    protected function error($message, $statusCode) {
+        return response()->json([
+            'message' => $message,
+            'status' => $statusCode
         ], $statusCode);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login',[\App\Http\Controllers\AuthController::class,'login']);
-Route::post('/register',[\App\Http\Controllers\AuthController::class,'register']);
+Route::post('/login',[\App\Http\Controllers\Api\AuthController::class,'login']);
+Route::post('/register',[\App\Http\Controllers\Api\AuthController::class,'register']);
 
-Route::get('/tickets', function () {
-    return \App\Models\Ticket::all();
-});
+Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
