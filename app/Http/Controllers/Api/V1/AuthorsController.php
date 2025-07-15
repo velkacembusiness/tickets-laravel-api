35fef4ab91bb350536fc94a2e0e28ee9c347fa54
VelKacem <velkacembusiness@gmail.com>
2025-07-15 09:15:21 +0100

    Using and  filtering nested resources

---
+++ app/Http/Controllers/Api/V1/AuthorsController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreUserRequest;
+use App\Http\Requests\Api\UpdateUserRequest;
+use App\Http\Resources\Api\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthorsController  extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if ($this->include('tickets')) {
+            return UserResource::collection(User::with('tickets')->paginate());
+        }
+
+        return UserResource::collection(User::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $author)
+    {
+        if ($this->include('tickets')) {
+            return new UserResource($author->load('tickets'));
+        }
+
+        return new UserResource($author);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\V1\AuthorsController;
+use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
@@ -11,7 +13,9 @@ Route::post('/register',[AuthController::class,'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
-Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
+//Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->apiResource('authors', AuthorsController::class);
+Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
